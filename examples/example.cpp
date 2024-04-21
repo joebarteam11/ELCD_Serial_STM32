16bf92d19c3dfd777d639946e2845df97123ff7b
joebarteam11 <micka.theot@gmail.com>
2024-04-21 12:50:49 +0200

    Adding examples folder

---
+++ examples/example.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+#include "eLCD_Serial.h"
+
+eLCD_Serial LCD(D1); // tx pin, no Rx required
+string test="Score :";
+
+int main()
+{
+    LCD.init();
+    LCD.clear();
+    LCD.cursor(false);
+    LCD.moveCursor(0,0);
+    LCD.print(test);
+    LCD.moveCursor(test.length(),0);
+    LCD.print(12345);
+
+}
